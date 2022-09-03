196ccf1d8c8cf2098980696dd3742eb561fcac4d
Animal <24845294+ItzOnlyAnimal@users.noreply.github.com>
2022-09-03 18:17:00 -0400

    fixed up some things

--- RestartButton/Main.cs
+++ RestartButton/Main.cs
@@ -14,63 +14,76 @@ using Main = RestartButton.Main;
 
 namespace RestartButton;
 
-public static class Guh {
+public static class Guh
+{
     public const string Name = "RestartButton";
     public const string Author = "Animal & Bluscream";
     public const string Version = "1.1.0";
     public const string DownloadLink = "https://github.com/Aniiiiiimal/CVRMods";
 }
 
-public class Main : MelonMod {
-    public const string bat_template = @"
+public class Main : MelonMod
+{
+    private const string BatTemplate = @"
 taskkill /f /im {0}
 timeout /t {1}
 start """" {2}
 ";
-    public MelonPreferences_Entry keybind;
-    public MelonPreferences_Entry vr_failsave;
 
-    public override void OnApplicationStart() {
-        MelonPreferences_Category cat = MelonPreferences.CreateCategory(Guh.Name);
-        keybind = cat.CreateEntry<KeyCode>("restart_bind", KeyCode.End, "Restart Key Bind", "Key to press to restart game");
-        vr_failsave = cat.CreateEntry<bool>("vr_failsave", false, "VR Failsave", "Failsave option to detect VR even if the command line arguments don't provide it");
-        ButtonAPI.OnInit += () => {
-            ChilloutButtonAPI.UI.SubMenu menu = ButtonAPI.MainPage.AddSubMenu("Restart Game");
-            _ = menu.AddButton("Restart", "Restart ChilloutVR", () => {
-                RestartGame();
-            });
+    private MelonPreferences_Entry keybind;
+    private MelonPreferences_Entry vrFailsafe;
+
+    private MelonLogger.Instance logger;
+
+    public override void OnApplicationStart()
+    {
+        logger = LoggerInstance;
+        var cat = MelonPreferences.CreateCategory(Guh.Name);
+        keybind = cat.CreateEntry("restart_bind", KeyCode.End, "Restart Key Bind",
+            "Key to press to restart game");
+        vrFailsafe = cat.CreateEntry("vr_failsave", false, "VR Failsafe",
+            "Failsafe option to detect VR even if the command line arguments don't provide it");
+        ButtonAPI.OnInit += () =>
+        {
+            _ = ButtonAPI.MainPage.AddButton("Restart", "Restart ChilloutVR", RestartGame);
         };
     }
 
-    public void RestartGame() {
-        LoggerInstance.Warning("Restarting!");
+    private void RestartGame()
+    {
+        logger.Warning("Restarting!");
         MelonPreferences.Save();
-        string filename = Process.GetCurrentProcess().ProcessName + ".exe";
-        // var filepath = $"{Environment.CurrentDirectory}/{filename}";
-        string args = string.Join(" ", Environment.GetCommandLineArgs());
+        var filename = $"{Process.GetCurrentProcess().ProcessName}.exe";
+        var args = $"\"{string.Join(" ", Environment.GetCommandLineArgs())}\"";
         // MelonLogger.Warning(PlayerSetup.Instance._inVr ? "IN VR" : "NOT IN VR");
         // MelonLogger.Warning(PlayerSetup.Instance.vrHeadTracker.activeSelf ? "VR HEADSET ACTIVE" : "VR HEADSET INACTIVE");
-        MelonLogger.Warning("Game start args: " + args);
-        if ((bool)vr_failsave.BoxedValue && PlayerSetup.Instance._inVr) {
+        logger.Warning("Game start args: " + args);
+        if ((bool) vrFailsafe.BoxedValue && PlayerSetup.Instance._inVr)
+        {
             args += " -vr";
         }
-
-        File.WriteAllText("restart.bat", string.Format(bat_template, filename, 3, args.Replace("%", "%%")));
-        _ = Process.Start(new ProcessStartInfo() { FileName = "restart.bat", CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden });
+        
+        File.WriteAllText("restart.bat", string.Format(BatTemplate, filename, 3, args.Replace("%", "%%")));
+        _ = Process.Start(new ProcessStartInfo
+            {FileName = "restart.bat", CreateNoWindow = true, WindowStyle = ProcessWindowStyle.Hidden});
         RootLogic.Instance.QuitApplication();
         Environment.Exit(0);
     }
 
-    public override void OnUpdate() {
-        if (!Input.GetKeyDown((KeyCode)keybind.BoxedValue)) {
+    public override void OnUpdate()
+    {
+        if (!Input.GetKeyDown((KeyCode) keybind.BoxedValue))
+        {
             return;
         }
 
-        try {
+        try
+        {
             RestartGame();
-            /*Process.Start("cmd.exe", "/C taskkill /im ChilloutVR.exe");*/
-        } catch (Exception e) {
-            LoggerInstance.Error($"Failed to restart: {e}");
+        }
+        catch (Exception e)
+        {
+            logger.Error($"Failed to restart: {e}");
         }
     }
 }
